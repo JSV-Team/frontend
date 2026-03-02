c82a502fe19b61805851a62e0b819b987210965f
hoangnh1706 <0107hoanguyen@gmail.com>
2026-03-02 23:09:07 +0700

    update
    update frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home/Home";
-import EditProfilePage from "./pages/EditProfile/EditProfilePage";
-import ReputationPage from "./pages/Reputation/ReputationPage";
-import PostsPage from "./pages/Posts/PostsPage";
+import './App.css'
+import AppRouter from './routers/AppRouter'
 
-export default function App() {
+function App() {
   return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/edit" element={<EditProfilePage />} />
-        <Route path="/profile/reputation" element={<ReputationPage />} />
-        <Route path="/profile/posts" element={<PostsPage />} />
-      </Routes>
-    </MainLayout>
-  );
+    <AppRouter />
+  )
 }
+
+export default App
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/Home/Home';
 import Match from '../pages/Match/Match';
 import Friends from '../pages/Friends/Friends';
+import EditProfilePage from '../pages/EditProfile/EditProfilePage';
+import ReputationPage from '../pages/Reputation/ReputationPage';
+import PostsPage from '../pages/Posts/PostsPage';
+
 function AppRouter() {
   return (
     <Router>
@@ -12,6 +16,9 @@ function AppRouter() {
           <Route path="/" element={<Home />} />
           <Route path="/match" element={<Match />} />
           <Route path="/friends" element={<Friends />} />
+          <Route path="/profile/edit" element={<EditProfilePage />} />
+          <Route path="/profile/reputation" element={<ReputationPage />} />
+          <Route path="/profile/posts" element={<PostsPage />} />
         </Routes>
       </MainLayout>
     </Router>
