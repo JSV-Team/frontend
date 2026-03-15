91c67c6734d8da32d3edda16910278f42bf9c474
haison1234 <sonnguyen556699@gmail.com>
2026-03-15 15:35:26 +0700

    Update AppRouter.jsx

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -9,7 +9,7 @@ import Login from '../pages/Login/Login';
 import Landing from '../pages/Landing/Landing';
 
 // Bổ sung dòng import Anh Bảo Vệ vào đây
-import ProtectedRoute from './ProtectedRoute'; 
+import ProtectedRoute from './ProtectedRoute';
 
 function AppRouter() {
   return (
@@ -17,31 +17,31 @@ function AppRouter() {
       <Routes>
         {/* Trang công khai - không cần đăng nhập */}
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} /> 
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         {/* Các trang VIP: Phải qua Bảo Vệ -> Mặc Đồng Phục (MainLayout) -> Vào Phòng (Home/Match/...) */}
         <Route path="/home" element={
-            <ProtectedRoute>
-                <MainLayout><Home /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Home /></MainLayout>
+          </ProtectedRoute>
         } />
-        
+
         <Route path="/match" element={
-            <ProtectedRoute>
-                <MainLayout><Match /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Match /></MainLayout>
+          </ProtectedRoute>
         } />
-        
+
         <Route path="/friends" element={
-            <ProtectedRoute>
-                <MainLayout><Friends /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Friends /></MainLayout>
+          </ProtectedRoute>
         } />
-        
+
         <Route path="/notifications" element={
-            <ProtectedRoute>
-                <MainLayout><Notifications /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Notifications /></MainLayout>
+          </ProtectedRoute>
         } />
       </Routes>
     </Router>
