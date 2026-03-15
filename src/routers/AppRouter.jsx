928b13fc8fdceb5b14a545310fd46a1b2f87ffee
haison1234 <sonnguyen556699@gmail.com>
2026-03-15 15:54:31 +0700

    Update AppRouter.jsx

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -8,38 +8,38 @@ import Notifications from '../pages/Notifications/Notifications';
 import Login from '../pages/Login/Login';
 
 // Bổ sung dòng import Anh Bảo Vệ vào đây
-import ProtectedRoute from './ProtectedRoute'; 
+import ProtectedRoute from './ProtectedRoute';
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         {/* Trang thả rông: Ai vào cũng được */}
-        <Route path="/login" element={<Login />} /> 
+        <Route path="/login" element={<Login />} />
 
         {/* Các trang VIP: Phải qua Bảo Vệ -> Mặc Đồng Phục (MainLayout) -> Vào Phòng (Home/Match/...) */}
         <Route path="/" element={
-            <ProtectedRoute>
-                <MainLayout><Home /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Home /></MainLayout>
+          </ProtectedRoute>
         } />
-        
+
         <Route path="/match" element={
-            <ProtectedRoute>
-                <MainLayout><Match /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Match /></MainLayout>
+          </ProtectedRoute>
         } />
-        
+
         <Route path="/friends" element={
-            <ProtectedRoute>
-                <MainLayout><Friends /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Friends /></MainLayout>
+          </ProtectedRoute>
         } />
-        
+
         <Route path="/notifications" element={
-            <ProtectedRoute>
-                <MainLayout><Notifications /></MainLayout>
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <MainLayout><Notifications /></MainLayout>
+          </ProtectedRoute>
         } />
       </Routes>
     </Router>
