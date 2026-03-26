fb8d50194d7a6b8fe44b4ac6d4a250ee8440d04e
hoangnh1706 <0107hoanguyen@gmail.com>
2026-03-26 21:28:11 +0700

    Update Notifications.jsx

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -4,6 +4,7 @@ import useNotifications from '../../hooks/useNotifications';
 import { useTheme } from '../../contexts/ThemeContext';
 import Particles from '../../components/Particles/Particles';
 import Aurora from '../../components/Aurora/Aurora';
+import Grainient from '../../components/Grainient/Grainient';
 
 function Notifications() {
   const { theme } = useTheme();
@@ -14,7 +15,6 @@ function Notifications() {
     refreshNotifications,
     markAsRead
   } = useNotifications();
-  const { theme } = useTheme();
 
   useEffect(() => {
     refreshNotifications();
