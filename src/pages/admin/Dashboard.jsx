011d63af6007976232323083b953c13a68d46b2c
hoangnh1706 <0107hoanguyen@gmail.com>
2026-03-28 15:28:23 +0700

    add

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -148,6 +148,7 @@ const Dashboard = () => {
                     itemStyle={{ color: '#2563eb', fontWeight: 600 }}
                   />
                   <Line
+                    name="User"
                     type="monotone"
                     dataKey="value"
                     stroke="#2563eb"
